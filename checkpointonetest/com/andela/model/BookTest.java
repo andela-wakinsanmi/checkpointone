fdb6d19531cccd869c858bc8c4d73449c0434385
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2015-12-20 13:20:09 +0100

    [Start #110462394] Creates Book Model

--- checkpointonetest/com/andela/model/BookTest.java
+++ checkpointonetest/com/andela/model/BookTest.java
@@ -1,10 +1,19 @@
 package com.andela.model;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Spykins on 20/12/2015.
  */
 public class BookTest {
-
+  Book bookTest = new Book("Lion King", 1024, "Waleola", 1);
+  @Test
+  public void testIncrementBookCopies() throws Exception {
+    int increment = 5;
+    int initialNumberOfCopies = bookTest.getNumberOfCopies();
+    bookTest.incrementBookCopies(increment);
+    assertTrue(bookTest.getNumberOfCopies() == initialNumberOfCopies + increment);
+  }
 }
--- src/com/andela/model/Book.java
+++ src/com/andela/model/Book.java
@@ -4,4 +4,36 @@ package com.andela.model;
  * Created by Spykins on 20/12/2015.
  */
 public class Book {
+  private String bookTitle;
+  private int isbnNumber;
+  private String nameOfAuthor;
+  private int numberOfCopies;
+
+  public Book(String bookTitle, int isbnNumber, String nameOfAuthor, int numberOfCopies) {
+    this.bookTitle = bookTitle;
+    this.isbnNumber = isbnNumber;
+    this.nameOfAuthor = nameOfAuthor;
+    this.numberOfCopies = numberOfCopies;
+  }
+  public String getBookTitle() {
+    return bookTitle;
+  }
+  public int getIsbnNumber() {
+    return isbnNumber;
+  }
+
+  public String getNameOfAuthor() {
+    return nameOfAuthor;
+  }
+
+  public int getNumberOfCopies() {
+    return numberOfCopies;
+  }
+
+  public void setBookTitle(String bookTitle) {
+    this.bookTitle = bookTitle;
+  }
+  public void incrementBookCopies(int numberOfCopiesToAdd){
+    this.numberOfCopies += numberOfCopiesToAdd;
+  }
 }
