ba14f4915a5a2ddd23360352d6a868d1fc5122ff
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2015-12-22 05:20:44 +0100

    [#110463496] Creates Student Class

--- src/com/andela/member/Student.java
+++ src/com/andela/member/Student.java
@@ -1,7 +1,34 @@
 package com.andela.member;
 
+import com.andela.model.Member;
+
 /**
  * Created by Spykins on 21/12/2015.
  */
-public class Student {
+public class Student extends Member {
+  private String studentDepartment;
+  private String studentSubjecctOfInterest;
+  public Student (String memberName, int memberIdNumber, String memberAddress){
+    super(memberName, memberIdNumber, memberAddress);
+  }
+  public Student(String memberName, int memberIdNumber){
+    super(memberName,memberIdNumber);
+  }
+
+  public String getStudentDepartment() {
+    return studentDepartment;
+  }
+
+  public void setStudentDepartment(String studentDepartment) {
+    this.studentDepartment = studentDepartment;
+  }
+
+  public String getStudentSubjecctOfInterest() {
+    return studentSubjecctOfInterest;
+  }
+
+  public void setStudentSubjecctOfInterest(String studentSubjecctOfInterest) {
+    this.studentSubjecctOfInterest = studentSubjecctOfInterest;
+  }
 }
+
