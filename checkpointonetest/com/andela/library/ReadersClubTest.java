c7a9eb12645c280cc1c6c563b30e1316ca64d734
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2015-12-23 04:14:02 +0100

    [Starts #110583748] Creates ReadersClub and ReadersClubTest file

---
+++ checkpointonetest/com/andela/library/ReadersClubTest.java
@@ -0,0 +1,36 @@
+package com.andela.library;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Spykins on 23/12/2015.
+ */
+public class ReadersClubTest {
+
+  @Test
+  public void testRequestForBook() throws Exception {
+
+  }
+
+  @Test
+  public void testAddBookToLibrary() throws Exception {
+
+  }
+
+  @Test
+  public void testAddBooksToLibrary() throws Exception {
+
+  }
+
+  @Test
+  public void testAddMemberToLibrary() throws Exception {
+
+  }
+
+  @Test
+  public void testAddMembersToLibrary() throws Exception {
+
+  }
+}
---
+++ src/com/andela/library/ReadersClub.java
@@ -0,0 +1,74 @@
+package com.andela.library;
+
+import com.andela.model.Book;
+import com.andela.model.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Spykins on 23/12/2015.
+ */
+public class ReadersClub {
+  private List<Member>allClubMember;
+  private List<Book>allBooksInLibrary;
+  private Map<Book, Integer>allBooksAvailableToBorrow;
+  private Map<Member, List<Book>>memberAndBooksBorrowed;
+  BookQueue bookQueue;
+
+  public ReadersClub(BookQueue bookQueue, Member newMember, Book newBook){
+    this.bookQueue = bookQueue;
+    initializePrivateVariables();
+    allClubMember.add(newMember);
+    allBooksInLibrary.add(newBook);
+    //add book to bookAvailableToCopy
+  }
+  public ReadersClub(BookQueue bookqueue, Member[] newMembers, Book[] newBooks){
+    this.bookQueue = bookqueue;
+    initializePrivateVariables();
+    for(Member newMember : newMembers)
+      allClubMember.add(newMember);
+    for(Book newBook : newBooks)
+      allBooksInLibrary.add(newBook);
+    //add book to bookAvailableToCopy
+  }
+  private void initializePrivateVariables(){
+    allClubMember = new ArrayList<Member>();
+    allBooksInLibrary = new ArrayList<Book>();
+    allBooksAvailableToBorrow = new HashMap<Book, Integer>();
+    memberAndBooksBorrowed = new HashMap<Member, List<Book>>();
+  }
+  private void addBookToBookAvailableToBorrow(Book newBookToAdd){
+
+  }
+  private void addBooksToBookAvailableToBorrow(Book[] newBookToAdd){
+
+  }
+  public Book requestForBook(Member memberRequesting, Book bookMemberRequestsFor){
+
+    return null;
+  }
+  private boolean isMemberAvailableInLibrary(Member memberRequesting){
+    return true;
+  }
+  private boolean isBookAvailableInLibrary(Book bookRequestingFor){
+    return true;
+  }
+  private boolean isBookAvailableToBorrow(Book bookRequestingFor){
+    return true;
+  }
+  public void addBookToLibrary(Book...newBook){
+
+  }
+  public void addBooksToLibrary(Book[] newBooks){
+
+  }
+  public void addMemberToLibrary(Member...newMember){
+
+  }
+  public void addMembersToLibrary(Member[] newMembers){
+
+  }
+}
